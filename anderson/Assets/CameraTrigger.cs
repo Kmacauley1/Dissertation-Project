1c45821f74c99e3eeca2472bfc905f90b3f30631
K Macauley <44847853+Kmacauley1@users.noreply.github.com>
2022-05-10 19:40:46 +0100

    Camera/Trigger/environment
    Added camera systems, triggers, beta environment.

---
+++ anderson/Assets/CameraTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+
+    
+{
+    [Tooltip("Put Camera you want to activate here")]
+    public GameObject cameraOn;
+    [Tooltip("Put Camera you want to deactivate here")]
+    public GameObject cameraOff;
+    
+   
+    void OnTriggerEnter(Collider other)
+    {
+        //check for object tag
+        if (other.tag == "Player")
+        {
+            // turns off whatever camera is in off and turns on the cam in on
+            Debug.Log("Collided");
+            cameraOn.SetActive(true);
+            cameraOff.SetActive(false);
+            
+        }
+    }
+
+   
+   
+}
