89ccde77e92e65be4f7fddf1a08a1572414655bf
K Macauley <44847853+Kmacauley1@users.noreply.github.com>
2022-05-16 19:16:51 +0100

    Implemented Dialogue Manager
    Put in dialogue UI

---
+++ anderson/Assets/NPC_Data.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "TYPE NPC NAME HERE", menuName = "NPC Dialogue Data")]
+    public class NPC_Data : ScriptableObject
+    {
+        public new string name;
+        public bool playerInRange;
+        public float maxRange = 5f;
+        public TextAsset inkJSON;
+        
+        //we use this index to call npc specific functions in NPC_Dialogue script
+        //make sure each NPC index is unique to avoid calling func more than once
+        public int Index;
+    }
+}
